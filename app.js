a71250ceadf6a12c4c2d3a31166ae5611ca17ae3
unknown <TuanNT22@117TuanNT22.fsoft.fpt.vn>
2015-08-10 17:06:09 +0700

    New chat app

---
+++ app.js
@@ -0,0 +1,30 @@
+/**
+ * Created by TuanNT22 on 10-08-2015.
+ */
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    fs = require('fs');
+app.listen(3000);
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+io.sockets.on('connection', function (socket) {
+    //Each client has unique socket id.
+    console.log('The actual socket.id is: ', socket.id);
+    //Send message to new client.
+    socket.emit('news', 'Wellcome to chat!');
+    //Handle new message from client and broadcast.
+    socket.on('new-mess', function(data){
+        io.sockets.emit('broadcast', data);
+    });
+});
+
+
